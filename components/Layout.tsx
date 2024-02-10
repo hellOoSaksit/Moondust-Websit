5c461a80e6c663d6e4d791943f946c9985a2c60b
Saksit <hello.saksit1999@gmail.com>
2024-02-10 23:59:55 +0700

    NewUpdate

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,5 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Sidebar from "./Sidebar"
 
 export default function Layout({ children }) {
   return (
@@ -8,7 +7,7 @@ export default function Layout({ children }) {
     <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
           <div className="bg-gradient-to-r from-violet-200 to-pink-200" style={{ flex: 1 }}>
             <Navbar />
-            <Sidebar>{children}</Sidebar>
+            {children}
           </div>
               <div className="bg-gradient-to-r from-blue-200 to-cyan-200">
                 <Footer />
